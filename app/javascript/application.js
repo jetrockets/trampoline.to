a0e158adbc2c3bb2482df86ec86841bcb4a5855b
Alexey Solilin <solilin@jetrockets.com>
2022-02-14 23:01:36 +0300

    Tune cssbundling-rails and jsbundling-rails

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,19 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+import { Application } from '@hotwired/stimulus';
+import { Turbo } from '@hotwired/turbo-rails';
+import Rails from '@rails/ujs';
+
+import {
+  Dropdown,
+  Toggle,
+  Modal
+} from 'tailwindcss-stimulus-components';
+
+const application = Application.start();
+// application.debug = false
+window.Stimulus = application;
+application.register('dropdown', Dropdown);
+application.register('modal', Modal);
+application.register('toggle', Toggle);
+
+Rails.start();
+Turbo.setProgressBarDelay(500);
