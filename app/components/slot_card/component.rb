0225e263ebcd9c039505ac4a846233caea7ee6ff
Maxim Romanov <romanov.maxim.98@gmail.com>
2022-02-25 14:46:18 +0300

    Fixes (#29)
    Co-authored-by: Maksim Romanov <maksim.romanov@jetrockets.pro>

---
+++ app/components/slot_card/component.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class SlotCard::Component < ApplicationComponent
+  # def initialize(name:, description:, tag: nil, classes: nil, img_url: 'logo-textless.svg', **options)
+  #   super(tag:, classes:, **options)
+  #   @name = name
+  #   @description = description
+  #   @img_url = img_url
+  # end
+end
